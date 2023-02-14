f74802524740b423f58c7042e719bf23bd8a10c5
Pritesh1511 <125392659+Pritesh1511@users.noreply.github.com>
2023-02-14 15:31:41 -0800

    dasd

--- Ass1.py
+++ Ass1.py
@@ -4,14 +4,5 @@ import streamlit as st
 st.write("APp")
 df = pd.read_csv("Data Manipulation Worksheet.xlsx","Financing Table" , parse_dates=['Date'],index_col=['Date'])
 
-begDate = df.index.min()
-endDate = df.index.max()
-minDateDropdown = pd.to_datetime('2007-01-01')
-pickStart =st.sidebar.date_input("Pick start date:",begDate,min_value = minDateDropdown)
-pickEnd = st.sidebar.date_input("Pick start date:",endDate)
-
-
-filterDF = df.loc[pickStart:pickEnd]
-st.write(filterDF)
 
 
