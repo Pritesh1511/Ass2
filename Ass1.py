de2f9a0ae79985f6aa04be333c58d138cbdd0ffb
Pritesh1511 <125392659+Pritesh1511@users.noreply.github.com>
2023-02-14 15:18:18 -0800

    asas

--- Ass1.py
+++ Ass1.py
@@ -6,7 +6,7 @@ st.write("APP WORKING")
 df = pd.read_csv("Data Maniupulation Worksheet.xlsx",sheet_name = 'Financing Table')
 begDate = df.index.min()
 endDate = df.index.max()
-minDateDropdown = pd.to_datetime('2010-01-01')
+minDateDropdown = pd.to_datetime('2007-01-01')
 pickStart =st.sidebar.date_input("Pick start date:",begDate,min_value = minDateDropdown)
 pickEnd = st.sidebar.date_input("Pick start date:",endDate)
 
