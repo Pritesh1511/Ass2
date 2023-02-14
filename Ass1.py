95a30120d5a5df3d23f78e2be871655c4516e7fb
Pritesh1511 <125392659+Pritesh1511@users.noreply.github.com>
2023-02-14 15:36:49 -0800

    dasd

--- Ass1.py
+++ Ass1.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 st.write("APp")
-df = pd.read_csv("Data Manipulation Worksheet.xlsx",sheet_name = "Financing Table")
+df = pd.read_csv("Data Manipulation Worksheet.xlsx")
 
 
 
