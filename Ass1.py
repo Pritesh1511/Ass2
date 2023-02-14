b558b7ce6aed91c0ae67642e2dbb44ff12c57c65
Pritesh1511 <125392659+Pritesh1511@users.noreply.github.com>
2023-02-14 15:36:10 -0800

    sdasda

--- Ass1.py
+++ Ass1.py
@@ -2,7 +2,8 @@ import pandas as pd
 import streamlit as st
 
 st.write("APp")
-df = pd.read_csv("Data Manipulation Worksheet.xlsx",sheet_name = "Financing Table" , parse_dates=['Date'],index_col=['Date'])
+df = pd.read_csv("Data Manipulation Worksheet.xlsx",sheet_name = "Financing Table")
+
 print(df)
 
 
