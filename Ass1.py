dc3f9b475bc91621c1a89ce1780c9d7932f46823
Pritesh1511 <125392659+Pritesh1511@users.noreply.github.com>
2023-02-14 15:25:02 -0800

    sdsd

--- Ass1.py
+++ Ass1.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import streamlit as st
 
-
 st.write("APP WORKING")
-df = pd.read_csv("Data Maniupulation Worksheet.xlsx",sheet_name = 'Financing Table')
+df = pd.read_csv("Data Maniupulation Worksheet.xlsx",sheet_name = 'Financing Table',parse_dates=['Date'],index_col=['Date'])
+
 begDate = df.index.min()
 endDate = df.index.max()
 minDateDropdown = pd.to_datetime('2007-01-01')
 pickStart =st.sidebar.date_input("Pick start date:",begDate,min_value = minDateDropdown)
 pickEnd = st.sidebar.date_input("Pick start date:",endDate)
+
 
 filterDF = df.loc[pickStart:pickEnd]
 st.write(filterDF)
